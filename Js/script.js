815f96c8fe36d963ac7e414ece2d698388bf928c
Mahdi <mahdi1nap@gmail.com>
2025-09-08 11:03:04 +0330

    add new swiper

--- Js/script.js
+++ Js/script.js
@@ -12,3 +12,25 @@
         992: { slidesPerView: 5 },
       },
     });
+
+    const swiperTwo = new Swiper(".category-carouselTwo", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".next2",
+    prevEl: ".prev2",
+  },
+  breakpoints: {
+    0: { slidesPerView: 2 },
+    576: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    reverseDirection: true,
+  },
+  speed: 800,
+});
+
