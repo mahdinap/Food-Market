48dd8fb17835454997bd9629e8adf8e33b78dcf1
Mahdi <mahdi1nap@gmail.com>
2025-09-13 18:32:23 +0330

    add Page Transition Loader

--- Js/script.js
+++ Js/script.js
@@ -49,3 +49,15 @@ const swiperThree = new Swiper(".category-carouselThree", {
   },
   
 });
+
+
+window.addEventListener('load', function() {
+  const loader = document.getElementById('page-loader');
+  loader.style.opacity = '1';
+  setTimeout(() => {
+    loader.style.transition = 'opacity 0.5s ease';
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 500);
+  }, 300);
+});
+
